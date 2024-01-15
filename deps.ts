b0dc352c3646df65981bb63b85a07ff1feb22901
sgohlke <gohlke.stefan@yahoo.de>
2024-01-15 21:02:19 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 export {
    assertStringIncludes,
-} from 'https://deno.land/std@0.208.0/assert/assert_string_includes.ts'
+} from 'https://deno.land/std@0.212.0/assert/assert_string_includes.ts'
 
-export { fail } from 'https://deno.land/std@0.208.0/assert/fail.ts'
+export { fail } from 'https://deno.land/std@0.212.0/assert/fail.ts'
 
 export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/deps.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/deps.ts'
 
 export {
    logAndReturnErrorResponse,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/response/ResponseHeaders.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/ResponseHeaders.ts'
