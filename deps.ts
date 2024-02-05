9cf53b597c8696f377e6a211885c6d1534166da4
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 21:57:08 +0100

    Update deps and unstable flags

--- deps.ts
+++ deps.ts
@@ -1,12 +1,8 @@
 export {
-   assertStringIncludes,
-} from 'https://deno.land/std@0.212.0/assert/assert_string_includes.ts'
-
-export { fail } from 'https://deno.land/std@0.212.0/assert/fail.ts'
-
-export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/deps.ts'
+   assertStringIncludes,
+   fail
+} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
 
 export {
    logAndReturnErrorResponse,
