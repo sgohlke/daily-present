819c8e3fc1182f0a528aa6d6c71131083f9986f6
sgohlke <gohlke.stefan@yahoo.de>
2024-02-25 15:54:47 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -7,5 +7,5 @@ export {
 export {
    logAndReturnErrorResponse,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/ResponseHeaders.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.2/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.2/response/ResponseHeaders.ts'
